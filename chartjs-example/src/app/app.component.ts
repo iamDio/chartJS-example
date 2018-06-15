1b661493c3329ee85e55f472b2839f26742f91c6
iamDio <claudio.villani00@gmail.com>
2018-06-14 21:49:16 -0400

    initiated app and chartjs

---
+++ chartjs-example/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements ngOnInit {
+  title = 'app';
+
+ngOnInit() {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+      // The data for our dataset
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45],
+          }]
+      },
+      // Configuration options go here
+      options: {}
+  });
+}
+
+}
